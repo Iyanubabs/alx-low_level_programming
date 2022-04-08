204fc334347f2c7ebe98b72125d690b179f26e19
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-08 09:01:37 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,10 @@
  */
 int main(void)
 {
+
 int n, last;
+
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
@@ -21,5 +24,6 @@ else if (last == 0)
 printf("Last digit of %d is %d and is 0\n", n, last);
 else if (last < 6 && last !=0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
 return (0);
 }
