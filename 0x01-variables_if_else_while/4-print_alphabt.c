6b4385cdfb123d065d88f6170e332912f9994324
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-08 15:58:48 -0700

    letters of alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,17 +3,18 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print the letters of the alphabet except e, q
+ * Description: print the letters of the alphabets except e, q
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
 
 int main(void)
-{ int i = 97;
+{
+	int i = 97;
 
 	while (i < 123)
 	{
-		if(i != 101 && i != 113)
+		if(i != 101 && i !=113)
 		{
 			putchar(i);
 		}
@@ -21,5 +22,5 @@ int main(void)
 	}
 	putchar(10);
 
-	return(0);
+	return (0);
 }
