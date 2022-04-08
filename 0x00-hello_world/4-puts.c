8d91029b7a18af6bb746ca9f5e348313ce683d29
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-07 17:57:56 -0700

    1st C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(void)	
-	{
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+int main(void)
+{
+
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
-	}
+
+}
