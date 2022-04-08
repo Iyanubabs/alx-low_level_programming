5bf9a7e34dfef2a02663def7aa123bc8bee55d4e
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-07 19:41:25 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Discription: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
  * Return: Always 0 (Success)
  */
