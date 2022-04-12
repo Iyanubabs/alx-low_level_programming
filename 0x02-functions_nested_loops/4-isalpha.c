9a319be81d7b73cefc0a23f1447bd6d6e0c22422
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-12 16:24:27 -0700

    function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 
 /**
  *
- *  * print_sign - Print the sign of a number
+ * print_sign - Print the sign of a number
  *
- *   * @n: type int integer, can be negative or positive
+ * @n: type int integer, can be negative or positive
  *
- *    * Description: print +, 0, or - depending on number, along with return
+ * Description: print +, 0, or - depending on number, along with return
  *
- *     * Return: 1 if +, 0 if 0, and -1 if -
+ * Return: 1 if +, 0 if 0, and -1 if -
  *
- *      */
+ */
 
 
 
