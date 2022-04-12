27eafb23f74648202b37eb0feac71ef2faa4e8d8
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-12 16:42:07 -0700

    ok

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,21 +10,10 @@
  * Return: 1 if +, 0 if 0, and -1 if -
  */
 
-int print_sign(int n)
+int _isalpha(int c)
 {
-	if (n > 0)
-	{
-		_putchar('+');
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
 	else
-	{
-		_putchar('0');
 		return (0);
-	}
 }
