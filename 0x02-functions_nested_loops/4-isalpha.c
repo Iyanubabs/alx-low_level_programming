3c7edf83c0e171bf24b309d9d12a24ce7150ce0e
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-12 16:31:52 -0700

    go

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_sign - Print the sign of a number
  *
  * @n: type int integer, can be negative or positive
@@ -9,13 +8,9 @@
  * Description: print +, 0, or - depending on number, along with return
  *
  * Return: 1 if +, 0 if 0, and -1 if -
- *
  */
 
-
-
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
@@ -27,7 +22,7 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar('0');
 		return (0);
