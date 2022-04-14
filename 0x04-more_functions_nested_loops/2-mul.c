ef5a99f994d2fe4c2a3e77bd83031a6d82ba62ff
Babatunde Iyanuoluwa <babatundeiyanuoluwa1@gmail.com>
2022-04-14 07:48:58 -0700

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * multiplies two integers
+ * mul - multiplies two integers
  * @a: int to be multiplied to b
  * @b: int to be multiplied to a
  * Return: the result of the operation
  */
-
 int mul(int a, int b)
 {
 	int c;
